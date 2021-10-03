fae8ed98481b479fa1966ce8bcbd28c84f7bca6f
Phan Cong Long <pcl2k2@gmail.com>
2021-10-03 18:14:39 +0700

    Initial code

---
+++ src/store/cart-context.js
@@ -0,0 +1,8 @@
+import React from "react";
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  removeitem: (id) => {},
+});
+export default CartContext;
